f792c22204d66f47fb170adf0498fe7069f49f3a
mspz0125 <zhangcheng.a@myai.tech>
2024-02-29 12:04:56 +0800

    push

--- fe/src/pages/components/ForkMeBadge.tsx
+++ fe/src/pages/components/ForkMeBadge.tsx
@@ -2,22 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 const ForkMeBadge: React.FC = () => {
-  return (
-    <a
-      href="https://github.com/Likenttt/cloudflare-worker-short-url"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <div style={{ position: "fixed", top: 0, right: 0, zIndex: 1000 }}>
-        <Image
-          src="/forkme_right_darkblue.webp"
-          alt="Fork me on GitHub"
-          width={149}
-          height={149}
-        />
-      </div>
-    </a>
-  );
+  return (<span></span>);
 };
 
 export default ForkMeBadge;
